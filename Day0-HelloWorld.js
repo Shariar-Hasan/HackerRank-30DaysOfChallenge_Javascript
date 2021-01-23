aa0922a3a3fc460afb84cc593c1819baf9f2e6e9
ethico <shahriar.hasan.1523@gmail.com>
2021-01-23 22:13:13 +0600

    Day-0

---
+++ Day0-HelloWorld.js
@@ -0,0 +1,14 @@
+function processData(inputString) {
+    // This line of code prints the first line of output
+    console.log("Hello, World.");
+    console.log(inputString);
+
+    // Write the second line of output that prints the contents of 'inputString' here.
+}
+function main() {
+    var getValue = readline();
+    processData(getValue);
+}
+
+
+
